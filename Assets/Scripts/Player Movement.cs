65af9dbc0891ecb917422158acf63e8470296061
baktirsukru <101118829+baktirsukru@users.noreply.github.com>
2025-01-31 19:20:13 +0300

    Initial commit

---
+++ Assets/Scripts/Player Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float thrustForce = 5f;
+    [SerializeField] float maxYSpeed = 5f;
+    
+    Rigidbody2D rb;
+    
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Clamp(rb.linearVelocity.y + thrustForce * Time.deltaTime, -maxYSpeed, maxYSpeed));
+            Debug.Log("Space key was pressed");
+        }
+    }
+}
