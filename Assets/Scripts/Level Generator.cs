e6232a1ea06997522e986cb7ad8834510127f8ad
baktirsukru <101118829+baktirsukru@users.noreply.github.com>
2025-02-04 18:17:44 +0300

    Level Generation

--- Assets/Scripts/Level Generator.cs
+++ Assets/Scripts/Level Generator.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -5,33 +7,63 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject platformPrefab;
     [SerializeField] int startingNumberOfPlatforms = 3;
     [SerializeField] Transform platformParent;
-    [SerializeField] float platformSize = 2f;
+    [SerializeField] float platformSize = 20f;
+    [SerializeField] float platformSpeed = 5f;
 
+    GameObject[] platforms = new GameObject[3];
     void Start()
     {
-        GenerateLevel();
+        GeneratePlatforms();
     }
 
-    void GenerateLevel()
+    void Update()
+    {
+        MovePlatforms();
+    }
+
+    void GeneratePlatforms()
     {
         for (int i = 0; i < startingNumberOfPlatforms; i++)
         {
-            float startPositionX;
-
-            if (i == 0)
-            {
-                startPositionX = transform.position.x;
-            }
-            else
-            {
-                startPositionX = transform.position.x + i * platformSize;
-
-            }
+            float startPositionX = SpawnPositionCalculation(i);
 
             Vector2 spawnPosition = new Vector2(startPositionX, transform.position.y);
-            Instantiate(platformPrefab, spawnPosition, Quaternion.identity, platformParent);
-            
+            GameObject newPlatform = Instantiate(platformPrefab, spawnPosition, Quaternion.identity, platformParent);
+
+            platforms[i] = newPlatform;
         }
-        
+    }
+
+    private float SpawnPositionCalculation(int i)
+    {
+        float startPositionX;
+
+        if (i == 0)
+        {
+            startPositionX = transform.position.x;
+        }
+        else
+        {
+            startPositionX = transform.position.x + i * platformSize;
+        }
+
+        return startPositionX;
+    }
+
+    void MovePlatforms()
+    {
+        for (int i = 0; i < platforms.Length; i++)
+        {
+            GameObject currentPlatform = platforms[i];
+
+            platforms[i].transform.position = new Vector2(platforms[i].transform.position.x - platformSpeed * Time.deltaTime, platforms[i].transform.position.y);
+
+            if (platforms[i].transform.position.x < -platformSize)
+            {
+                /* platforms.(currentPlatform);
+                Destroy(currentPlatform);
+                GeneratePlatforms(); */
+            }
+        }
     }
 }
